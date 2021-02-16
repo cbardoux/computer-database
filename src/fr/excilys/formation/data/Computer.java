7f8bfa0c679740cd3017ac6f2228dca1b807750d
Clément Bardoux <cbardoux@excilys.com>
2021-02-16 10:53:06 +0100

    Data creation (Computer & Company)

---
+++ src/fr/excilys/formation/data/Computer.java
@@ -0,0 +1,54 @@
+package fr.excilys.formation.data;
+
+import java.time.LocalDate;
+
+public class Computer {
+	private int id;
+	private String name;
+	private LocalDate introduced;
+	private LocalDate discontinued;
+	private int company_id;
+	
+	public Computer() {}
+	
+	public Computer(int id, String name, LocalDate introduced, LocalDate discontinued, int company_id) {
+		this.id = id;
+		this.name = name;
+		this.introduced = introduced;
+		this.discontinued = discontinued;
+		this.company_id = company_id;
+	}
+
+	public int getCompany_id() {
+		return company_id;
+	}
+
+	public void setCompany_id(int company_id) {
+		this.company_id = company_id;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public LocalDate getIntroduced() {
+		return introduced;
+	}
+	public void setIntroduced(LocalDate introduced) {
+		this.introduced = introduced;
+	}
+	public LocalDate getDiscontinued() {
+		return discontinued;
+	}
+	public void setDiscontinued(LocalDate discontinued) {
+		this.discontinued = discontinued;
+	}
+}
